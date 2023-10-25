988c9db96ffc5dc034aa8f6ae5342a58ea92b0d9
Danny Vaca <danny270793@gmail.com>
2023-10-25 11:08:45 -0500

    publish image locally

--- ci/dagger/src/index.ts
+++ ci/dagger/src/index.ts
@@ -14,8 +14,12 @@ const version: string = '1.0.0'
 connect(async (client: Client) => {
   const jmixPath: string = Path.join(process.cwd(), '..', '..', 'code', 'jmix')
   const jmixContextDir: Directory = client.host().directory(jmixPath)
-  const path: string = `${dockerName}:${version}.tar`
+  const path: string = `${dockerName}:${version}`
   await jmixContextDir.dockerBuild().export(path)
-  bash(`docker load < ${path}`)
-  Fs.unlinkSync(path)
+
+  const output: string = bash(`docker image load < ${path}.tar`)
+  Fs.unlinkSync(`${path}.tar`)
+  const outputs: string[] = output.trim().split(':')
+  const id: string = outputs[outputs.length - 1]
+  bash(`docker image tag ${id} ${dockerName}:${version}`)
 }, { LogOutput: process.stderr })
